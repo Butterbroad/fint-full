d19d6d036fa370fb89e79b1b53ca593ccf3a914f
butterbroad <pyatochevskiy@gmail.com>
2020-10-16 17:48:04 +0300

    finished hero

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,6 +1,5 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', () => {
-
   //get link with parameters
   const paramLinks = document.querySelectorAll('.get-link');
   if (paramLinks.length > 0) {
@@ -16,12 +15,35 @@ document.addEventListener('DOMContentLoaded', () => {
     footerBtn.classList.contains('active') ? footerBtn.innerText = "Скрыть" : footerBtn.innerText = "Регистрация";
     const footer = document.querySelector('.footer');
     footer.classList.toggle('active');
+    document.body.classList.toggle('lock');
   });
 
 
 
+  //scroll test
+  // let counter = 0;
 
+  // window.addEventListener('wheel', e => {
+  //   const lol = document.querySelector('.lol')
 
+  //   const delta = e.deltaY || e.detail || e.wheelDelta;
+  //   console.log(delta)
+
+  //   delta >= 0 ? counter++ : counter--;
+
+  //   console.log(counter)
+
+  //   switch (counter) {
+  //     case 0:
+  //       lol.classList.remove('active');
+  //       break;
+  //     case 1:
+  //       lol.classList.add('active');
+  //       const logo = document.querySelector('.header__logo');
+  //       logo.classList.add('white')
+  //       break;
+  //   }
+  // });
 
 
 
